877028fa8113e5fd8b52c869307ec5d2d84af661
Love-Fromage <mathecool22@gmail.com>
2023-03-04 23:17:47 -0500

    ajout d'un question pour le last name

--- src/App.java
+++ src/App.java
@@ -9,7 +9,8 @@ public class App {
         String sex = ChooseSex(scanner);
         String firstName = ChooseFirstName(scanner);
         System.out.println(sex + " " + firstName);
-
+        boolean wantLastName = askLastName(scanner);
+        System.out.println(sex + " " + firstName + " " + wantLastName);
         scanner.close();
     }
 
@@ -20,13 +21,14 @@ public class App {
         System.out.print("Your choice:");
 
         int selection = scanner.nextInt();
-        scanner.nextLine(); // ici on viens "annuler le enter Enter sinon ca bug dans le choose name tantot"
+        // scanner.nextLine(); // ici on viens "annuler le enter Enter sinon ca bug dans le choose name tantot"
+        System.in.
 
         switch (selection) {
             case 0:
-                return "Male 😀";
+                return "Male 👨‍🦱";
             case 1:
-                return "Female";
+                return "Female 👩‍🦰";
             default:
                 System.out.println("Please choose between the two values");
                 return ChooseSex(scanner);
@@ -36,5 +38,24 @@ public class App {
     public static String ChooseFirstName(Scanner scanner) {
         System.out.print("Please choose a first name : ");
         return scanner.nextLine();
+    }
+
+    public static boolean askLastName(Scanner scanner) {
+        System.out.println("Would you like to choose a last name?");
+        System.out.println("[0] : Yes");
+        System.out.println("[1] : No");
+
+        int selection = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (selection) {
+            case 0:
+                return true;
+            case 1:
+                return false;
+            default:
+                System.out.println("Please choose between the two values");
+                return askLastName(scanner);
+        }
     }
 }
