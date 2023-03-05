3153ba742dd6c2bab46293c426b01de3507102bf
Love-Fromage <mathecool22@gmail.com>
2023-03-04 20:39:21 -0500

    clean up pour la class Person

--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,9 @@
 public class Person {
-   String[] sexPossible = {"Male","Female"};
    String sex;
    String firstName;
    String lastName;
    public Person(int sexChoice, String fName) {
-        sex = sexPossible[sexChoice];
+      //   sex = sexPossible[sexChoice];
         firstName = fName;
    }
    
